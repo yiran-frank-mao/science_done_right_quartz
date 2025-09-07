213e550a0ece9f500c79981dd55db33bef2e362b
ravenclawfrank <ravenclawfrank@gmail.com>
2025-09-07 11:21:07 +0000

    update layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      globalGraph: {
+        showTags: false,
+      }
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
